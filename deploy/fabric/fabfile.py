c7dcd79ba7cc0b8b660d3340fdfa1a979fb4a3b7
liuliang <qq79402005@gmail.com>
2018-01-10 14:59:59 +0800

    使用ansible发布网站 #3
    还是fabric最简单易用

---
+++ deploy/fabric/fabfile.py
@@ -0,0 +1,14 @@
+import os
+from fabric.api import *
+from fabric.context_managers import *
+from fabric.contrib.console import confirm
+
+env.user = "root"
+env.hosts = ["120.25.122.107"]
+
+def upload_web():
+    run("sudo rm -r /opt/tomcat/webapps/ROOT")
+    run("sudo mkdir /opt/tomcat/webapps/ROOT")
+    put(os.getcwd() + "/../../web/out/artifacts/web_war_exploded/RES", "/opt/tomcat/webapps/ROOT/", use_sudo=True)
+    put(os.getcwd() + "/../../web/out/artifacts/web_war_exploded/WEB-INF", "/opt/tomcat/webapps/ROOT/", use_sudo=True)
+    run("sudo systemctl restart tomcat")
